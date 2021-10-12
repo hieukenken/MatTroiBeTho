ca3810b2a5a40d45cd415cf3f0ef0bfadc1cc5cc
hieukenken <ph17032000@outlook.com>
2021-10-12 23:35:24 +0700

    Tạo login cho admin

---
+++ WebProgramingMatTroiBeTho.WebApp/Areas/Admin/Commons/UserSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProgramingMatTroiBeTho.WebApp.Areas.Admin.Commons
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+    }
+}
--- WebProgramingMatTroiBeTho.WebApp/Controllers/HomeController.cs
+++ WebProgramingMatTroiBeTho.WebApp/Controllers/HomeController.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebProgramingMatTroiBeTho.Models.Models;
+using WebProgramingMatTroiBeTho.WebApp.Areas.Admin.Commons;
 
 namespace WebProgramingMatTroiBeTho.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+
+      
         string err = string.Empty;
         int rows = 0;
         // GET: Home
         public ActionResult Index()
         {
-            var baiviet = new BaiVietDB().GetBaiViet(ref err);
-            return View(baiviet);
+            if (SessionHelper.GetSession() != null) {
+                var baiviet = new BaiVietDB().GetBaiViet(ref err);
+                return View(baiviet);
+            }
+            else
+            {
+               return RedirectToAction("Index", "Login", new { Area = "Admin" });
+            }
+            
         }
     }
 }
