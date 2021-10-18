b2984083b58f928f3a6a6056d17a124a7560c503
Hùng Nguyễn Thế <hungvip36555@gmail.com>
2021-10-18 22:29:55 +0700

    tạo xong class

---
+++ WebProgramingMatTroiBeTho.Category/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProgramingMatTroiBeTho.Category
+{
+    internal class Category
+    {
+        public string  MaSP { get; set; }
+        public string  IDAnh { get; set; }
+        public string TenSP { get; set; }
+        public string DinhDanhSP { get; set; }
+        public float DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public string ChiTietSP { get; set; }
+        public int MaDVT { get; set; }
+        public string LoaiSP { get; set; }
+        public bool TinhTrang { get; set; }
+    }
+}
