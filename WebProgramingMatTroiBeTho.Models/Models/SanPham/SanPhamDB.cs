9dfecdb5b1c5effd4db37331c5070a9a8b2f84aa
Be-png <56946002+Be-png@users.noreply.github.com>
2021-11-07 12:00:53 +0700

    xem sua xoa sp

--- WebProgramingMatTroiBeTho.Models/Models/SanPham/SanPhamDB.cs
+++ WebProgramingMatTroiBeTho.Models/Models/SanPham/SanPhamDB.cs
@@ -27,7 +27,7 @@ namespace WebProgramingMatTroiBeTho.Models.Models.SanPham
                     list.Add(
                         new SanPham()
                         {
-                            MaSP = dataReader["MaSP"].ToString(),
+                            MaSP =( dataReader["MaSP"].ToString()),
                             TenSP = dataReader["TenSP"].ToString(),
                             DonGia = double.Parse(dataReader["DonGia"].ToString()),
                             SoLuong = int.Parse(dataReader["SoLuong"].ToString()),
@@ -39,8 +39,56 @@ namespace WebProgramingMatTroiBeTho.Models.Models.SanPham
                 }
             }
             
-
             return list;
+        }
+        
+        public SanPham XemChiTietSP(ref string err, string id)
+        {
+            SqlDataReader dataReader = data.MyExecuteReader(ref err, "PSH_Select_SanPhamByID", CommandType.StoredProcedure,
+                new SqlParameter("@id",id));
+            SanPham sanPham =new SanPham() ;
+            if (dataReader != null)
+            {
+                while (dataReader.Read())
+                {
+                    sanPham = new SanPham()
+                    {
+                        MaSP = dataReader["MaSP"].ToString(),
+                        TenSP = dataReader["TenSP"].ToString(),
+                        DonGia = double.Parse(dataReader["DonGia"].ToString()),
+                        SoLuong = int.Parse(dataReader["SoLuong"].ToString()),
+                        ChiTietSanPham = dataReader["ChiTietSanPham"].ToString(),
+                        LoaiSP = dataReader["LoaiSP"].ToString(),
+                        TinhTrang = bool.Parse(dataReader["TinhTrang"].ToString()),
+                        HinhAnh = dataReader["HinhAnh"].ToString()
+                    };
+                }
+            }
+            return sanPham;
+        }
+        
+
+       
+        public bool InsertUpdateSanPham(ref string err, ref int rows, SanPham sanPham)
+        {
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@id",sanPham.MaSP),                 
+                    new SqlParameter("@TenSP",sanPham.TenSP),
+                    new SqlParameter("@IDAnh",sanPham.HinhAnh),
+                      new SqlParameter("@DonGia",sanPham.DonGia),
+                        new SqlParameter("@SoLuong",sanPham.SoLuong),
+                          new SqlParameter("@ChiTietSanPham",sanPham.ChiTietSanPham),
+                            new SqlParameter("@LoaiSP",sanPham.LoaiSP),
+                            new SqlParameter("@TinhTrang",sanPham.TinhTrang)
+            };
+            return data.MyExecteNonQuery(ref err, ref rows, "NHP_SanPham_ADD", CommandType.StoredProcedure, param);
+        }
+        public bool DeleteSanPham(ref string err, ref int rows, SanPham sanPham)
+        {
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@id",sanPham.MaSP)
+            };
+            return data.MyExecteNonQuery(ref err, ref rows, "NHP_SanPham_DeleteByID", CommandType.StoredProcedure, param);
         }
     }
 }
